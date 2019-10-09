d47a9e7061d32ae78019472763372ac3431f9038
Steven <mail@stevendunn.co.uk>
2019-10-09 08:17:44 +0100

    Update allelefinder.sh

--- allelefinder.sh
+++ allelefinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep $1 *.tab | cut -f 2,3,4,5,11 | sed '/snp/!d' | sort | uniq -c | sort -r -s -n -k 1,1
+grep $1 *.tab | cut -f 2,3,4,5,11 | sed '/snp/!d' | sort | uniq -c | sort -r -s -n -k 1,1 | sed -e 's/ /\t/g' | tr -s '[[:blank:]]' | cut -f1 --complement
