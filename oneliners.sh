f869a6a27d0ffe8952b0e98a388035bc3f6e3c1a
Steven <mail@stevendunn.co.uk>
2022-05-17 09:30:43 +0100

    Update oneliners.sh

--- oneliners.sh
+++ oneliners.sh
@@ -2,9 +2,12 @@
 
 # Query gene ontology (GO) database with list of GO-terms
 # GO terms assigned to annotation using eggnog mapper
-# Unique GO terms extracted from output using bash and stored to file "go_uniq.csv"
+# Unique GO terms extracted from output and stored to file "go_uniq.csv"
 # Loop looks for GO term ID, returns an additional line which encodes function, and encodes it next to the ID for parsing
-while read -r line; do grep -m1 --after-context 1 'id: '$line go.obo | sed 'N;s/\nname:/,/g' ; done < go_uniq.csv > go_terms_resolved.csv
+# go.obo can be downloaded here: http://purl.obolibrary.org/obo/go.obo
+# Note that the database needs to be preprocessed to prevent spurious matches to alt_ids:
+# grep -v 'alt_id:' go.obo > go.obo2
+while read -r line; do grep -m1 --after-context 1 'id: '$line go.obo2 | sed 'N;s/\nname:/,/g' ; done < go_uniq.csv > go_terms_resolved.csv
 
 # Shovill assembly loop
 # Assumes directory containing forward and reverse reads that encode sample ID
